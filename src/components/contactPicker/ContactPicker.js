593851d28c2672b7bc7c9eb7f787805f47f6ab3c
zeebers7475 <zeebers7475@gmail.com>
2024-12-07 05:13:37 -0500

    Finished Project

--- src/components/contactPicker/ContactPicker.js
+++ src/components/contactPicker/ContactPicker.js
@@ -4,9 +4,11 @@ export const ContactPicker = (props) => {
   return (
     <>
       <select name="contact" id="contact" onChange={props.setContact} >
+      <option selected value="No Contact Selected">No Contact Selected</option>
         {props.contacts.map((contact) => {
           return (
-            <option value={contact.name}>{contact.name}</option>)
+            <option value={contact.name}>{contact.name}</option>
+          )
           })
         }
       </select>
